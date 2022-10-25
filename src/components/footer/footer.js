70006040937ad542d9342eea0e6919d415a2d4d0
Sallein <saleinkyl@gmail.com>
2022-10-25 23:33:44 +0300

    создал компонент Footer пустой

---
+++ src/components/footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+import style from './footer.module.css'
+
+
+const Footer = () => {
+
+    return(
+        <div className={style.wrap}>
+            <div className={style.bg}>
+
+            </div>
+        </div>
+    )
+}
+
+
+export default Footer
