04691c0c4379e2c06d1ee3feec5f57cce18545b7
Sallein <saleinkyl@gmail.com>
2022-11-05 19:08:18 +0300

    рефакторинг main компоненты

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import style from './App.module.css'
 import Main from './components/main/main'
-import Header from './components/header/Header'
 import About from './components/about_us/about'
 import Discription from './components/discription/discription'
 import Products from './components/products/products'
@@ -16,7 +15,6 @@ function App() {
   return (
     <div className={style.app}>
       <Main />
-      <Header />
       <About />
       <Discription />
       <h1 className={style.title}>Продукция</h1>
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -7,9 +7,21 @@ const Main = () => {
 
   return(
     <div className={style.main}>
+      <div className={style.wrap}>
+        <div>
+          <h2 className={style.h2}>ЧПУП ​​​"ЛЕСАКА"</h2>
+          <p className={style.p}>Дзержинский район <br/> д.Станьково, ул. Войсковая 4</p>
+        </div>
+        <div>
+          <ul className={style.contacts}>
+            <li>lesaka212525@gmail.com</li>
+            <li>+375 (29) 772 62 50</li>
+            <li>+375 (29) 768 86 54</li>
+          </ul>
+        </div>
+      </div>
       <h1 className={style.title}>ДРОВА <br/> колотые</h1>
       <p className={style.miniTitle}>Реализуем доставку дров Минск, Минский и Дзержинский районы</p>
-      <span className={style.stick}></span>
     </div>
   )
 }
