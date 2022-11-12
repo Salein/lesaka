85b476d7da25c2d8bdc01b770b16c42091c76aaa
Sallein <saleinkyl@gmail.com>
2022-11-12 15:31:07 +0300

    добавил хук media для mobile version

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -3,15 +3,17 @@ import Conifers from './Conifers/conifers'
 import Hardwoods from './Hardwoods/hardwoods'
 import Mixed from './Mixed/mixed'
 import style from './products.module.css'
-
+import { useMediaQuery } from 'react-responsive'
 
 const Products = () => {
 
+  const isDesktop = useMediaQuery({ query: '(min-width: 700px)' }) 
+  const isMobile = useMediaQuery({ query: '(max-width: 500px)' })
   return(
     <div className={style.products}>
-      <Hardwoods />
-      <Conifers />
-      <Mixed />
+      {isDesktop && <Hardwoods />} || {isMobile && <Hardwoods />}
+      {isDesktop && <Conifers />}
+     {isDesktop && <Mixed />}
     </div>
   )
 }
